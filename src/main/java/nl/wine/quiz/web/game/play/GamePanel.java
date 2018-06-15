924f9443a8a89c2fd254d9a528143d9f7fb275a9
Johanwv <johan_joto@hotmail.com>
2018-06-15 17:33:05 +0200

    Answers change onSubmit

--- src/main/java/nl/wine/quiz/web/game/play/GamePanel.java
+++ src/main/java/nl/wine/quiz/web/game/play/GamePanel.java
@@ -1,42 +1,57 @@
 package nl.wine.quiz.web.game.play;
 
-import nl.wine.quiz.dto.MultipleChoiceQuestions;
+import nl.wine.quiz.dto.MultipleChoiceQuestion;
 import nl.wine.quiz.dto.Option;
 import nl.wine.quiz.util.ModelUtil;
 import nl.wine.quiz.web.game.start.StartGamePage;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class GamePanel extends Panel
 {
+    private List<MultipleChoiceQuestion> questions;
+
     private List<Option> options;
 
     private String answer;
 
+    private int counter;
 
-    public GamePanel(String id)
+    private Form<List<Option>> questionForm;
+
+    public GamePanel(String id, IModel<List<MultipleChoiceQuestion>> multipleChoiceQuestionsModel)
     {
         super(id);
 
-        Form<Void> optionsForm = new Form<>("optionsForm");
+        questions = multipleChoiceQuestionsModel.getObject();
 
-        options = getQuestions().get(0).getOptions();
+        options = multipleChoiceQuestionsModel.getObject().get(counter).getOptions();
 
         answer = determineAnswer(options);
 
-        optionsForm.add(getOptionButton("optionA", 0));
-        optionsForm.add(getOptionButton("optionB", 1));
-        optionsForm.add(getOptionButton("optionC", 2));
-        optionsForm.add(getOptionButton("optionD", 3));
-        add(optionsForm);
+        questionForm = new Form<>("optionsForm", ModelUtil.listIModel(options));
+        questionForm.setOutputMarkupId(true);
+
+        RadioGroup<List<Option>> group = new RadioGroup<>("multipleChoiceQuestion", ModelUtil.listIModel(options));
+        group.add(getOptionButton("optionA", 0));
+        group.add(getOptionButton("optionB", 1));
+        group.add(getOptionButton("optionC", 2));
+        group.add(getOptionButton("optionD", 3));
+
+        questionForm.add(group);
+
+        Label numberOfQuestionsLabel = new Label("counter", new PropertyModel<>(this, "counter"));
+        numberOfQuestionsLabel.setOutputMarkupId(true);
+        questionForm.add(numberOfQuestionsLabel);
+        add(questionForm);
     }
 
     private String determineAnswer(List<Option> options)
@@ -45,51 +60,30 @@ public class GamePanel extends Panel
         return option.getOption();
     }
 
-    private List<MultipleChoiceQuestions> getQuestions()
-    {
-        List<Option> options = new ArrayList<>();
-        Option optionA = new Option();
-        optionA.setAnswer(true);
-        optionA.setOption("A");
-
-        Option optionB = new Option();
-        optionB.setAnswer(false);
-        optionB.setOption("B");
-
-        Option optionC = new Option();
-        optionC.setAnswer(false);
-        optionC.setOption("C");
-
-        Option optionD = new Option();
-        optionD.setAnswer(false);
-        optionD.setOption("D");
-
-        options.add(optionA);
-        options.add(optionB);
-        options.add(optionC);
-        options.add(optionD);
-
-        Collections.shuffle(options);
-
-        List<MultipleChoiceQuestions> questions = new ArrayList<>();
-
-        MultipleChoiceQuestions multipleChoiceQuestions = new MultipleChoiceQuestions();
-        multipleChoiceQuestions.setOptions(options);
-        multipleChoiceQuestions.setQuestion("Wat is the answer?");
-
-        questions.add(multipleChoiceQuestions);
-        return questions;
-    }
-
     private AjaxButton getOptionButton(String id, int optionNumber)
     {
         IModel model = new PropertyModel<>(ModelUtil.createModel(options.get(optionNumber)), "option");
-        AjaxButton ajaxButton = new AjaxButton(id, model)
+        AjaxButton ajaxButton = new AjaxButton(id, model, questionForm)
         {
             @Override
             protected void onSubmit(AjaxRequestTarget target)
             {
-                setResponsePage(StartGamePage.class);
+                if (++counter < questions.size())
+                {
+
+                    List<Option> newOptions = questions.get(counter).getOptions();
+                    for (int i = 0; i < 4; i++)
+                    {
+                        options.get(i).setAnswer(newOptions.get(i).isAnswer());
+                        options.get(i).setOption(newOptions.get(i).getOption());
+                    }
+                    answer = determineAnswer(newOptions);
+
+                    target.add(questionForm);
+                } else
+                {
+                    setResponsePage(StartGamePage.class);
+                }
             }
         };
         return ajaxButton;
