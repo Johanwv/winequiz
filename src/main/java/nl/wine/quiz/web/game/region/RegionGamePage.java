e511e56e4af598071a48d2e02518be237df9a733
Johanwv <johan_joto@hotmail.com>
2018-06-15 13:07:47 +0200

    Created MultipleChoice and Option

--- src/main/java/nl/wine/quiz/web/game/region/RegionGamePage.java
+++ src/main/java/nl/wine/quiz/web/game/region/RegionGamePage.java
@@ -1,22 +1,64 @@
 package nl.wine.quiz.web.game.region;
 
 
+import nl.wine.quiz.dto.MultipleChoice;
+import nl.wine.quiz.dto.Option;
 import nl.wine.quiz.web.base.BaseWineQuizPage;
 import nl.wine.quiz.web.game.StartGamePage;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Form;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegionGamePage extends BaseWineQuizPage
 {
+    private List<MultipleChoice> options;
+
     public RegionGamePage()
     {
+        options = getOptions();
+
         Form<Void> optionsForm = new Form<>("optionsForm");
         optionsForm.add(getStopButton("optionA"));
         optionsForm.add(getStopButton("optionB"));
         optionsForm.add(getStopButton("optionC"));
         optionsForm.add(getStopButton("optionD"));
         add(optionsForm);
+    }
+
+    private List<MultipleChoice> getOptions()
+    {
+        List<Option> options = new ArrayList<>();
+        Option optionA = new Option();
+        optionA.setAnswer(true);
+        optionA.setOption("A");
+
+        Option optionB = new Option();
+        optionA.setAnswer(false);
+        optionA.setOption("B");
+
+        Option optionC = new Option();
+        optionA.setAnswer(false);
+        optionA.setOption("C");
+
+        Option optionD = new Option();
+        optionA.setAnswer(false);
+        optionA.setOption("D");
+
+        options.add(optionA);
+        options.add(optionB);
+        options.add(optionC);
+        options.add(optionD);
+
+        List<MultipleChoice> multipleChoices = new ArrayList<>();
+
+        MultipleChoice multipleChoice = new MultipleChoice();
+        multipleChoice.setOptions(options);
+        multipleChoice.setQuestion("Wat is the answer?");
+
+        return multipleChoices;
     }
 
     private AjaxButton getStopButton(String id)
